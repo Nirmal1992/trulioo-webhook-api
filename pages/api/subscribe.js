f3303c01935455e5e944cbc7441431047ee53316
Nirmal Jain <jainnirmal1992@gmail.com>
2023-09-07 16:23:12 +0530

    callenge

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -3,7 +3,9 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
     console.log(req.body);
-    res.status(200).json(req.body);
+    res.status(200).json({
+      challenge: req?.body?.callenge || "",
+    });
   } else {
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
