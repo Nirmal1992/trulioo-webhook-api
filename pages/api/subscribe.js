734546f9aac466ddb2743d92ed75ce8b7f026465
Nirmal Jain <jainnirmal1992@gmail.com>
2023-09-07 16:13:19 +0530

    add subscribe api

---
+++ pages/api/subscribe.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    console.log(req.body);
+    res.status(200).json(req.body);
+  } else {
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
