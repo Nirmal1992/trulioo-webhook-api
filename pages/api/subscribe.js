3b862acdd5bdee9e34d6da66aaa4f9fdfd721682
Nirmal Jain <jainnirmal1992@gmail.com>
2023-09-13 11:11:02 +0530

    Update subscribe.js

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   if (req.method === "POST") {
     console.log(req.body);
     res.status(200).json({
-      challenge: req?.body?.callenge || "",
+      challenge: req?.body?.challenge || "",
     });
   } else {
     res.status(405).end(`Method ${req.method} Not Allowed`);
